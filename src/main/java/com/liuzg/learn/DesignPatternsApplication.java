cd91d3a7a04a2a0bc9a1a137691e708fdde98351
liuzg <1172727371@qq.com>
2018-11-19 17:39:21 +0800

    Initial commit

---
+++ src/main/java/com/liuzg/learn/DesignPatternsApplication.java
@@ -0,0 +1,19 @@
+package com.liuzg.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ *  Desc: SpringBoot启动类
+ * </pre>
+ * @author  liuzg
+ * @date  2018/11/19 16:58
+ */
+@SpringBootApplication
+public class DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsApplication.class, args);
+	}
+}
